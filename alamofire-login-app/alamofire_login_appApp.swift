3acbfdf5dcec64dedf2d8b486c7e797cb86cf5cd
moolnam <moolnam>
2022-05-31 14:39:26 +0900

    Initial Commit

---
+++ alamofire-login-app/alamofire_login_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  alamofire_login_appApp.swift
+//  alamofire-login-app
+//
+//  Created by KimJongHee on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct alamofire_login_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
